505dd1809e75c7a0d887ed823ff56cffdb75975c
Mir Ubaid Sadat <mubaid.acca@gmail.com>
2021-10-03 12:43:01 -0400

    Updated third file

--- src/setup/Third.java
+++ src/setup/Third.java
@@ -4,6 +4,9 @@ public class Third {
     public static void main(String[] args) {
 
         System.out.println("third file for self practice");
+
+        // this is a self practice as asked by professor
+
     }
 
 
